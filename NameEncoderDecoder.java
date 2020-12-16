fa1f3138369cf6c5b936629bfbd9de5f2067281b
Volodymyr-Duka <75455724+Volodymyr-Duka@users.noreply.github.com>
2020-12-16 22:51:59 +0200

    Add files via upload

---
+++ NameEncoderDecoder.java
@@ -0,0 +1,26 @@
+public class NameEncoderDecoder{
+    public String encode(String name){
+     name = "NOTFORYOU"+name+"YESNOTFORYOU"; 
+        return name.replace("e", "1")
+                .replace("u", "2")
+                .replace("i", "3")
+                .replace("o", "4")
+                .replace("a", "5");
+    
+    }
+    public String decode(String name){
+     String nameDecode = name;
+      nameDecode=nameDecode.replace("NOTFORYOU", "").replace("YES", "");
+        return nameDecode
+                
+                .replace("1", "e")
+                .replace("2", "u")
+                .replace("3", "i")
+                .replace("4", "o")
+                .replace("5", "a");
+    }
+
+public static void main(String[] args){
+NameEncoderDecoder decoder = new NameEncoderDecoder();
+    }
+}
